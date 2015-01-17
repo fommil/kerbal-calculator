6ca7cdfd3ebe8fb13572d3385ed90a0cbff20b5b
Sam Halliday <sam.halliday@gmail.com>
2015-01-17 21:46:58 +0000

    sanity check of deltav calculation

--- shared/src/test/scala/com/github/fommil/kerbal/SolverSuite.scala
+++ shared/src/test/scala/com/github/fommil/kerbal/SolverSuite.scala
@@ -7,7 +7,19 @@ object SolverSuite extends utest.TestSuite {
   private def bestEngines(solns: Stream[EngineSolution]): Set[String] =
     solns.sortBy(_.stageInitialMass).take(20).map(_.engine.name).toSet
 
+  private def similar(a: Double, b: Double, e: Double = 1e-06): Boolean = (a >= (b - e)) && (a <= (b + e))
+
   def tests = TestSuite {
+    "should calculate the correct deltav for a mainsail with orange tank"-{
+      // http://forum.kerbalspaceprogram.com/threads/107592-web-calculator-for-engines-fuel
+      val mainsail = Engines.Stock.engines.find(_.name.contains("Mainsail")).get
+      val orange = FuelTanks.Stock.tanks.find(_.name.contains("Jumbo-64")).get
+      val dv = EngineSolution(11, mainsail, 1, orange, 32, false, Nil).totalDeltaV
+
+      // 360 * 9.81 * math.log((11 + 6 + 36.0) / (11 + 6 + 4))
+      assert(similar(dv, 3269.447, 1.0))
+    }
+
     "should recommend sensible engines for a 10t payload from Kerbin to the Mun"-{
       val results = Solver.solve(1200, 10, 20, false, Large)
       val engines = bestEngines(results)
